32d1243eccd94f7205fc92cac1f0268e06d7b608
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2024-06-04 22:06:52 +0530

    feat: rendering list of users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 
+import { DUMMY_USERS } from './dummy-users';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,4 +12,6 @@ import { UserComponent } from './user/user.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+}
