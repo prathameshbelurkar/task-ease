be07ef4c049df05d7db6f09daaa9563770382602
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2024-06-04 11:11:11 +0530

    refactor: moved to directory

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { HeaderComponent } from './header.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
