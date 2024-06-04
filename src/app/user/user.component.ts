1a029df4f5b10bf8212b5b15fe5c5857e8e933eb
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2024-06-04 11:51:41 +0530

    feat: bind dynamic data to the users

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex];
 
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
 }
