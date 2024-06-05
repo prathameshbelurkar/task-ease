af8f244ff3ec1a8113dfe866125df7f1987796d4
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2024-06-05 16:55:15 +0530

    feat: find selected user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,26 @@ import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId: string = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
 
   onSelectUser(id: string) {
-    console.log('ID: ', id);
+    this.selectedUserId = id;
   }
 }
