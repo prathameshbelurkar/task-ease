25e7871568a9521a0287fa76207fe4f89a757a55
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2024-06-06 09:57:31 +0530

    feat: dynamic data integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ import { DUMMY_USERS } from './dummy-users';
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId: string = 'u1';
+  selectedUserId?: string;
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId)!;
