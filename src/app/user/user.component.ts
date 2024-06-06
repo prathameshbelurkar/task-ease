5b6975304feaf7b6b4d179c7d5a69ab0bc091887
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2024-06-06 16:06:48 +0530

    feat: rendering list for tasks for a user

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
-export interface User {
-  id: string;
-  name: string;
-  avatar: string;
-}
+import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
