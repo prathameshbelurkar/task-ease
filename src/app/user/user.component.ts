aa765e40e4282b88959eea92378b0a5591f6c645
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2024-06-08 15:39:53 +0530

    optimization(card): used wrapper for task component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,13 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+import { CardComponent } from '../shared/card/card.component';
+
 import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
